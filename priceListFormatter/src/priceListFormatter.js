b8e2940ed3dd81d030f9b2cc90acfe1385f42d52
adkuca <adkucauip@gmail.com>
2021-04-27 22:24:11 +0200

    test log being called

--- priceListFormatter/src/priceListFormatter.js
+++ priceListFormatter/src/priceListFormatter.js
@@ -1,5 +1,8 @@
 function priceListFormatter(data) {
   if (!Array.isArray(data) && !data.length) throw new Error();
+
+  console.log('');
+  // console.log('');
 }
 
 module.exports = priceListFormatter;
--- priceListFormatter/src/tests/priceListFormatter.test.js
+++ priceListFormatter/src/tests/priceListFormatter.test.js
@@ -1,4 +1,8 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const sinon = require('sinon');
+const { expect } = chai;
+chai.use(require('sinon-chai'));
+
 const priceListFormatter = require('../priceListFormatter.js');
 
 describe('#priceListFormatter()', function () {
@@ -17,6 +21,30 @@ describe('#priceListFormatter()', function () {
     });
     it('should throw error if argument is an empty array', function () {
       expect(() => dayOfYear([])).to.throw();
+    });
+  });
+
+  context('with correctly formatted data as input', function () {
+    beforeEach(() => {
+      sinon.restore();
+    });
+
+    const data = [
+      {
+        from: '2020-01-01',
+        to: '2020-02-01',
+        price: 34.5,
+      },
+    ];
+
+    it('should return undefined', function () {
+      expect(priceListFormatter(data)).to.be.an('undefined');
+    });
+
+    it('should log once', function () {
+      const log = sinon.spy(console, 'log');
+      priceListFormatter(data);
+      expect(log).to.be.calledOnce;
     });
   });
 });
