0bf681bbbe3dea6585b03c2b7e13107936cbc391
adkuca <adkucauip@gmail.com>
2021-04-27 19:54:23 +0200

    wrong input tests

--- priceListFormatter/src/priceListFormatter.js
+++ priceListFormatter/src/priceListFormatter.js
@@ -1,3 +1,5 @@
-function priceListFormatter(data) {}
+function priceListFormatter(data) {
+  if (!Array.isArray(data) && !data.length) throw new Error();
+}
 
 module.exports = priceListFormatter;
--- priceListFormatter/src/tests/priceListFormatter.test.js
+++ priceListFormatter/src/tests/priceListFormatter.test.js
@@ -5,4 +5,18 @@ describe('#priceListFormatter()', function () {
   it('should be a function', function () {
     expect(priceListFormatter).to.be.a('function');
   });
+
+  context('wrong input', function () {
+    it('should throw error if no argument', function () {
+      expect(() => dayOfYear(1)).to.throw();
+      expect(() => dayOfYear({})).to.throw();
+    });
+    it('should throw error if argument is not an array', function () {
+      expect(() => dayOfYear(1)).to.throw();
+      expect(() => dayOfYear({})).to.throw();
+    });
+    it('should throw error if argument is an empty array', function () {
+      expect(() => dayOfYear([])).to.throw();
+    });
+  });
 });
