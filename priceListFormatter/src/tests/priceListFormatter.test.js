5ce53114b5b3a63944242d4b929845094ac92616
adkuca <adkucauip@gmail.com>
2021-05-12 13:43:13 +0200

    fix function name in test

--- priceListFormatter/src/tests/priceListFormatter.test.js
+++ priceListFormatter/src/tests/priceListFormatter.test.js
@@ -12,15 +12,14 @@ describe('#priceListFormatter()', function () {
 
   context('wrong input', function () {
     it('should throw error if no argument', function () {
-      expect(() => dayOfYear(1)).to.throw();
-      expect(() => dayOfYear({})).to.throw();
+      expect(() => priceListFormatter()).to.throw();
     });
     it('should throw error if argument is not an array', function () {
-      expect(() => dayOfYear(1)).to.throw();
-      expect(() => dayOfYear({})).to.throw();
+      expect(() => priceListFormatter(1)).to.throw();
+      expect(() => priceListFormatter({})).to.throw();
     });
     it('should throw error if argument is an empty array', function () {
-      expect(() => dayOfYear([])).to.throw();
+      expect(() => priceListFormatter([])).to.throw();
     });
   });
 
